c3acfe6a5a771f35d01886d4073464edce35c590
asdnepr <suglobov83@gmail.com>
2021-06-12 03:14:00 +0300

    Commit Homework_7 alternative solution_update

--- Homework_7.py
+++ Homework_7.py
@@ -1,38 +1,9 @@
-# import random
-
 # 1.Задан некоторый список A содержащий целые числа. Разработать программу, которая вычисляет сумму элементов списка.
 A = [22, 33, 44, 55]
 my_sum = 0
 for i in A:
     my_sum += i
 print(my_sum)
-
-# def random_value():
-#     random_val = random.randrange(1, 10, 1)
-#     return random_val
-#
-#
-# def int_value():
-#     while True:
-#         my_value = input('Введите число от 0 до 9: ')
-#
-#         try:
-#             my_value = int(my_value)
-#
-#         except ValueError:
-#             print("Ошибка ввода! Попробуйте еще раз.")
-#
-#         else:
-#             return my_value
-#
-#
-# my_list = [random_value() for i in range(random_value())]
-# st_1 = 0
-# my_sum = 0
-# while st_1 < len(my_list):
-#     my_sum += my_list[st_1]
-#     st_1 += 1
-# print(f"Сумма элементов списка {my_list} равна: {my_sum}")
 
 # 2.Задан список строк. В каждой строке подсчитать количество вхождений заданного символа
 list_str = ['A mari usque ad mare.', 'Ab ovo usque ad mala.', 'Abiens abi!', 'Dictum est factum.']
@@ -41,13 +12,6 @@ my_input = input('Введите искомый символ: ')
 for i in list_str:
     print(f"Вхождений символа '{my_input}' в строку '{list_str[st_2]}' - {list_str[st_2].count(my_input)} ")
     st_2 += 1
-
-# list_str = ['A mari usque ad mare.', 'Ab ovo usque ad mala.', 'Abiens abi!', 'Dictum est factum.']
-# st_2 = 0
-# my_input = input('Введите искомый символ: ')
-# while st_2 < len(list_str):
-#     print(f"Вхождений символа '{my_input}' в строку '{list_str[st_2]}' - {list_str[st_2].count(my_input)} ")
-#     st_2 += 1
 
 # 3.Пользователь вводит число. Определение наличия заданного элемента в списке
 # list_=[2,8,3,4,3,5,2,1,0,3,4,4,5,8,7,7,5]. Если элемент не найден, то выводится соответствующее сообщение.
