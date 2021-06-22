edbaa0d0e9f2852f316ec1358dbc7c2b98f1a39c
asdnepr <suglobov83@gmail.com>
2021-06-22 18:15:15 +0300

    Commit Homework_9_update, all task is ready

--- Homework_10.py
+++ Homework_10.py
@@ -22,42 +22,14 @@ print(list_in_list)
 tuples_in_list = [tuple(i) for i in tuples_in_list]
 print(tuples_in_list)
 
-
-
-# list_1 = []
-# for item in dict_1.items(), dict_2.items():
-#     list_1.extend(item)
-#     print(type(item))
-# print(list_1)
-
-
-# Dict1 = {'color': ['red'], 'value': ['high']}
-# dict2 = {'color': ['yellow'], 'value': ['low']}
-#
-# # to store final result of dictionary
-# result = []
-#
-# # iterate through each dictionary in list
-# for dictionary in [dict_1, dict_2]:
-#     # iterate through key value of dictionary items
-#     for k, v in dictionary.items():
-#         # if key is already in result dictionary then extend new values
-#         result.extend(v)
-#         # else if key is not in dictionary then add key with the first value
-#
-#
-# print(result)
-# Результат:
-#
-# {'d': [10, 14, 13], 'a': [1, 2, 3, 4], 'b': [5, 8, 9, 0], 'c': [5, 6, 9]}
-
-
 # Задача 2
 # Дано словарь
 # a_dictionary = {"a": 1, "b": 2, "c": 3}
 # преобразовать его в список кортежей
 # [('a', 1), ('b', 2), ('c', 3)]
-
+a_dictionary = {"a": 1, "b": 2, "c": 3}
+print(a_dictionary)
+print(list(a_dictionary.items()))
 
 # Задача 3
 # Дано два списка
@@ -65,16 +37,26 @@ print(tuples_in_list)
 # list_b = [5, 6, 7, 8]
 # Создать из них список кортежей
 # list_c = [(1,5), (2,6), (3,7), (4,8)]
-
+list_a = [1, 2, 3, 4]
+list_b = [5, 6, 7, 8]
+list_c = [(list_a[i], list_b[i]) for i in range(len(list_a))]
+print(list_c)
 
 # Задача 4
 # Дано словарь
 # {1:'entry 1', 2:'entry 2', 3:'entry 3', 4:'entry 4'}
-# Создать кортеж занчений для первих трьох єлементов словоря
-
+# Создать кортеж занчений для первих трёх элементов словаря
+my_dict = {1: 'entry 1', 2: 'entry 2', 3: 'entry 3', 4: 'entry 4'}
+my_tuple = tuple(list(my_dict.values())[:3])
+print(my_tuple)
 
 # Задача 5
 # Дано список
 # ["bar", "baz", "qux", "etc"]
 # Создать кортеж
 # ("foo", "bar", "baz", "qux", "etc")
+my_list_2 = ["bar", "baz", "qux", "etc"]
+my_list_3 = ["foo"]
+my_list_3.extend(my_list_2)
+my_tuple_2 = tuple(my_list_3)
+print(my_tuple_2)
