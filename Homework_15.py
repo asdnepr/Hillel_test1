4457e1c1516f26250dcc74fafc6f9073d5160d63
asdnepr <suglobov83@gmail.com>
2021-07-14 02:04:14 +0300

    Commit Homework_15 ready

--- Homework_15.py
+++ Homework_15.py
@@ -1,6 +1,3 @@
-import json
-# from pprint import pprint
-
 # Задан файл HW.json
 # Обработать исходный файл таким образом, чтобы получить получить результирующий файл по подобию НW_result.json.
 # Весь код оформить в виде функций.
@@ -18,7 +15,7 @@ import json
 # 6. По значению определяете тип и сортируете по типу согласно результирующему json-ну.
 # 7. Генерируете все в новый словарь
 # 8. Словарь записываете в новый файл.
-
+# _____________________________________________________________________
 # Задачу из “Homework 11. Paбота с json обектом.” перевести в формат ООП.
 #
 # Класс должен содержать атрибути и методи. Атрибутами будут пути к исходному и результирующему файлам.
@@ -31,43 +28,64 @@ import json
 # Если придумаете еще больше методов будет хорошо. Помните о принципе “Единственной ответственности”.
 #
 # Екземпляр класса визивать через определенную точку входа python скрипта.
+import json
+import os
 
-with open('HW.json', 'r') as dict_data:
-    text = json.load(dict_data)
-my_list_int = []
-my_list_str = []
-my_list_float = []
-my_list_none_type = []
-my_list_bool = []
-my_dict = {}
-dict_data_dump = {}
-dict_data_2 = {}
-for item in text['employee']:
-    personName = item['firstName'] + ' ' + item['lastName']
-    for item_ in item.items():
-        if isinstance(item_[1], bool):
-            my_list_bool.append(item_[0])
-        elif isinstance(item_[1], int):
-            my_list_int.append(item_[0])
-        elif isinstance(item_[1], str):
-            my_list_str.append(item_[0])
-        elif isinstance(item_[1], float):
-            my_list_float.append(item_[0])
-        elif isinstance(item_[1], type(None)):
-            my_list_none_type.append(item_[0])
-    my_dict["int"] = my_list_int.copy()
-    my_dict["string"] = my_list_str.copy()
-    my_dict["float"] = my_list_float.copy()
-    my_dict["none_type"] = my_list_none_type.copy()
-    my_dict["bool"] = my_list_bool.copy()
-    my_list_int.clear()
-    my_list_str.clear()
-    my_list_float.clear()
-    my_list_none_type.clear()
-    my_list_bool.clear()
-    dict_data_dump[personName] = my_dict.copy()
-    my_dict.clear()
-dict_data_2['employee'] = dict_data_dump
-with open('HM_result.json', 'w') as f:
-    json.dump(dict_data_2, f, indent=3)
-# pprint(dict_data_2)
+
+class MyJsonClass:
+    def __init__(self, original_path=os.getcwd(), result_path=os.getcwd()):
+        self.result_ = {'employee': {}}
+        self.original_path = original_path
+        self.result_path = result_path
+        self.update_()
+
+    def original_file(self):
+        with open('HW.json', 'r') as self.original_path:
+            return json.loads(self.original_path.read())
+
+    def update_(self):
+        with open('HM_result.json', 'w') as new_json:
+            new_json.write(self.result_json())
+
+    @staticmethod
+    def type_comparison(dict_):
+        my_list_int = []
+        my_list_str = []
+        my_list_float = []
+        my_list_none_type = []
+        my_list_bool = []
+        my_dict = {}
+        for item_ in dict_.items():
+            if isinstance(item_[1], bool):
+                my_list_bool.append(item_[0])
+            elif isinstance(item_[1], int):
+                my_list_int.append(item_[0])
+            elif isinstance(item_[1], str):
+                my_list_str.append(item_[0])
+            elif isinstance(item_[1], float):
+                my_list_float.append(item_[0])
+            elif isinstance(item_[1], type(None)):
+                my_list_none_type.append(item_[0])
+        my_dict["int"] = my_list_int.copy()
+        my_dict["string"] = my_list_str.copy()
+        my_dict["float"] = my_list_float.copy()
+        my_dict["none_type"] = my_list_none_type.copy()
+        my_dict["bool"] = my_list_bool.copy()
+        my_list_int.clear()
+        my_list_str.clear()
+        my_list_float.clear()
+        my_list_none_type.clear()
+        my_list_bool.clear()
+        return my_dict
+
+    def result_json(self):
+        dict_data_dump = {}
+        for item in self.original_file()['employee']:
+            person_name = item['firstName'] + ' ' + item['lastName']
+            dict_data_dump[person_name] = self.type_comparison(item)
+        self.result_['employee'] = dict_data_dump
+        return json.dumps(self.result_, indent=3)
+
+
+if __name__ == '__main__':
+    outcome = MyJsonClass()
